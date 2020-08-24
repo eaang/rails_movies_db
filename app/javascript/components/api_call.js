31fa484b4fb40808c4ae604c258898cf0d81476f
Evan Ang <alethiologist@gmail.com>
2020-08-25 02:29:25 +0800

    gon is working

--- app/javascript/components/api_call.js
+++ app/javascript/components/api_call.js
@@ -1,7 +1,11 @@
 const apiCall = () => {
   const input = document.querySelector("#movie-test-input")
-  input.addEventListener("click", () => {
-    console.log("I'm clicked!")
+  const button = document.querySelector("#movie-test-button")
+  const url = `http://www.omdbapi.com/?apikey=${gon.omdb}&t=`
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const title = input.value.replace(/\s/g, '+');
+    console.log(url);
   })
 };
 
