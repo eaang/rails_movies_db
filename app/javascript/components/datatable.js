5b0e9c62d40e5e45fa69694f2422b26a1acc530b
Evan Ang <alethiologist@gmail.com>
2020-08-25 14:49:32 +0800

    fixed genretables

--- app/javascript/components/datatable.js
+++ app/javascript/components/datatable.js
@@ -7,8 +7,21 @@ const tableCode = () => {
   })
   .columns.adjust()
   .responsive.recalc();
+  const genreTable = $('#genretable').DataTable({
+    responsive: true,
+    stateSave: true,
+    "columns": [
+      null,
+      null,
+      null,
+      { "width": "55%" }
+    ]
+  })
+  .columns.adjust()
+  .responsive.recalc();
   document.addEventListener("turbolinks:before-cache", function() {
     dataTable.destroy();
+    genreTable.destroy();
   });
 }
 
