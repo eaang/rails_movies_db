b7bdb4d9401136582824a3be82704025079ebcd9
Evan Ang <alethiologist@gmail.com>
2020-08-27 17:05:48 +0800

    FUCKING RATINGS FUCKIN WORK

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,10 +1,10 @@
 class RatingsController < ApplicationController
   def change
-    @rating = Rating.find_or_create_by(rating_params)
+    @rating = Rating.find_or_create_by(movie_id: rating_params[:movie_id], user_id: rating_params[:user_id])
+    @rating.score = rating_params[:score]
     @movie = @rating.movie
     if @rating.save
       current_user.id == 1 ? (@my_score = @movie.evan_rating) : (@my_score = @movie.case_rating)
-      fail
       redirect_to @movie, notice: "Rating successfully updated."
     else
       render @movie
