a68c651f05ed0f48508fb04cc7574507b6c1e53e
Evan Ang <alethiologist@gmail.com>
2020-08-27 20:11:25 +0800

    fixed stat displaying wrongly

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -66,7 +66,7 @@ class MoviesController < ApplicationController
     @genres = Genre.all
     @genres_and_movies = Genre.joins(:movies)
     ratings = Rating.all.includes(:users)
-    @evan_reviews = ratings.where(user_id: 2)
+    @evan_reviews = ratings.where(user_id: 1)
     @case_reviews = ratings.where(user_id: 2)
     @top_rated = Movie.rated(5).sort_by { |e| e[:name] }
     @bottom_rated = Movie.rated(0).sort_by { |e| e[:name] }
